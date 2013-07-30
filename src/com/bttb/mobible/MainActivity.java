812e44475f7eeb54a3608b53abd58998a1195333
Dinesh Silva <sohandinesh@hotmail.com>
2013-07-30 11:20:56 +0530

    Blue highlight when scrolling pages
    Removing the blue highlight when scrolling pages beyond limit

--- src/com/bttb/mobible/MainActivity.java
+++ src/com/bttb/mobible/MainActivity.java
@@ -1,19 +1,23 @@
 package com.bttb.mobible;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.view.Menu;
+import android.view.View;
+
 import org.apache.cordova.DroidGap;
 
-public class MainActivity extends DroidGap {
+@SuppressLint({ "InlinedApi", "NewApi" }) public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD) {
+            this.appView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        }
         
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
