add0f11896fa2d7e68324aecf842c4fe95391944
Dinesh Silva <sohandinesh@hotmail.com>
2013-08-01 14:38:10 +0530

    Interface updates - Full screen
    Fixed full screen with no status bar

--- src/com/bttb/mobible/MainActivity.java
+++ src/com/bttb/mobible/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.view.Menu;
 import android.view.View;
-
+import android.view.WindowManager;
 import org.apache.cordova.DroidGap;
 
 @SuppressLint({ "InlinedApi", "NewApi" }) public class MainActivity extends DroidGap {
@@ -12,6 +12,8 @@ import org.apache.cordova.DroidGap;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.loadUrl("file:///android_asset/www/index.html");
         if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD) {
             this.appView.setOverScrollMode(View.OVER_SCROLL_NEVER);
